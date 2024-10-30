fccca7af8d58d6642e9860e90704ce7ae5ca3603
Taras Chuiko <tchuiko@griddynamics.com>
2024-10-30 13:07:24 +0000

    Completed the task

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,16 @@
-import IoCContainer from 'ioc-lite';
+import IoCContainer from "ioc-lite";
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from "../services/logger";
+import { HTTP } from "../services/http";
+import { Users } from "../services/users";
+import { IoCResources } from "src/types";
 
-export const createIoCContainer = () =>  {
-  const ioc = new IoCContainer();
-  // you can register some resources right now below...
+export const createIoCContainer = () => {
+  const ioc = new IoCContainer<IoCResources>();
+
+  ioc.registerClass("Logger", Logger);
+  ioc.registerClass("HTTP", HTTP);
+  ioc.registerClass("Users", Users);
 
   return ioc;
 };
